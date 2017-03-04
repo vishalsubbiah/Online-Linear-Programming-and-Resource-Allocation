f911aa31658ccfb80c853e953a8d467dbd90431a
gregdepaul <gdepaul@stanford.edu>
2017-03-03 18:59:27 -0800

    Created Larger SLPM

--- SLPM/SLPMOffline2.py
+++ SLPM/SLPMOffline2.py
@@ -1,22 +1,30 @@
-import numpy
+import numpy as np
 from cvxpy import *
 
 # Create two scalar optimization variables.
-X = Variable(5);
-z = Variable();
+m = 10
+K = 10000
 
-A = numpy.array([[1,0,1,1,0],[1,0,0,1,1],[1,0,1,1,0],[0,1,0,1,1],[0,0,1,0,0]]);
+A = np.random.randint(0, 2, (m, K))
 
-pi = numpy.array([0.75, 0.35, 0.4, 0.95, 0.75]);
+X = Variable(K)
+z = Variable()
+
+q = np.random.randint(10, 21, (K,1))
+p = np.random.uniform(0, 1, (m,1))
+pi = p.T.dot(A) + np.sqrt(0.2) * np.random.randn(1, K)
+
+A = np.random.randint(0, 2, (m, K))
+
 #randomVector = numpy.random.rand(1, 5);
 #scaledRandomVector = numpy.multiply(randomVector, 0.2);
 #pi = numpy.add(numpy.array([0.75, 0.35, 0.4, 0.95, 0.75]), scaledRandomVector);
 
-q = numpy.array([10, 5, 10, 10, 5]);
-
 constraints = [];
 
-for i in range(A.shape[1]):
+print A.shape
+
+for i in range(A.shape[0]):
     constraints.append( A[i,:]*X - z <= 0 );
 
     
