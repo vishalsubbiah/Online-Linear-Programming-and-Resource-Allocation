5ae9682d59c838d97c2d813234baa05c10731728
Vishal Subbiah <subbiahvishal@gmail.com>
2017-03-04 02:59:13 -0800

    online version of SCPM without for loop

--- SCPM-online/SCPM_o.py
+++ SCPM-online/SCPM_o.py
@@ -2,49 +2,44 @@ import numpy as np
 from cvxpy import *
 from random import random, randrange,uniform,normalvariate
 from math import sqrt
-# Create two scalar optimization variables.
 
-k=10
-m=2
 
-x = Variable(k)
-s = Variable(m)
+k=10000
+m=1
+n=1000
+x = Variable(k,1)
+s = Variable(m,1)
 z = Variable(1)
-pi=np.zeros((k))
-a=np.zeros((k,m))
-p=np.zeros(m)
-q=np.zeros(k)
+
+a = np.random.randint(0, 2, (k, m))
+p = np.random.uniform(0, 1, (m,1))
+q = np.random.randint(10, 21, (k,1))
+pi = a.dot(p) + np.sqrt(0.2) * np.random.randn(k, 1)
 w=1e-3
 
-
-
-for i in range(m):
-    p[i]=random()
-for i in range(k):
-    for j in range(m):
-        a[i,j]=randrange(0,2)
-    q[i]=randrange(10,21)
-
-for i in range(k):
-    pi[i]=sum(a[i,:]*p) + normalvariate(0,sqrt(0.2))
-
-
-constraints = []
-for l in range(k):
-    for i in range(m):
-        temp_sum=0
-        for j in range(k-1):
-            temp_sum=temp_sum+a[j,i]*x[j]
-        constraints.append(a[l,i]*x[l] - z + s[i] == -temp_sum)
-    constraints.append(x[l]<=q[l])
-    constraints.append(x[l]>=0)
-
+#part 1
+constraints = [x[n:] == 0,a.transpose()*x  - z + s == 0, x <= q, x >= 0, s >= 0]
 # Form objective.
-obj1 = Maximize(sum(pi*x) -z + w*sum(log(s))/m)
-obj2 = Maximize(sum(pi*x) -z + w*sum(1-exp(-s))/m)
+obj1 = Maximize(pi.transpose()*x -z + w*sum(log(s))/m)
+obj2 = Maximize(pi.transpose()*x -z + w*sum(1-exp(-s))/m)
 # Form and solve problem.
-prob = Problem(obj1, constraints)
-prob.solve()  # Returns the optimal value.
-print "status:", prob.status
-print "optimal value", prob.value
+prob1 = Problem(obj1, constraints)
+prob1.solve(solver=CVXOPT)  # Returns the optimal value.
+print "status:", prob1.status
+print "optimal value", prob1.value
+print "optimal var", x.value
+
+
+#part 2
+
+b = x[0:n]
+constraints = [x[0:n] - b == 0,a.transpose()*x  - z + s == 0, x <= q, x >= 0, s >= 0]
+obj1 = Maximize(pi.transpose()*x -z + w*sum(log(s))/m)
+obj2 = Maximize(pi.transpose()*x -z + w*sum(1-exp(-s))/m)
+# Form and solve problem.
+prob2 = Problem(obj1, constraints)
+prob2.solve(solver=CVXOPT,abstol=1e-25)  # Returns the optimal value.
+
+print "status:", prob2.status
+print "optimal value", prob2.value
 print "optimal var", x.value
